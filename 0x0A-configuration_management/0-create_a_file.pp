eb55c6690536cc4e4a405f5618ba388571f5b03f
Mulih <almulih@yahoo.com>
2024-02-23 04:18:35 -0800

    upload of task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#create a a puppet file
+
+file {'/tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}
+
