23255ca6ff9e6dce2b7d54cf90da82c605304f2e
Mulih <almulih@yahoo.com>
2024-02-26 07:54:40 +0300

    fix task 3 & 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 # Configure the SSH server to authenticates only through SSH keys.
 include stdlib
 
-file_line { 'Turn off passwd auth':
-  path   => '/home/ubuntu/.ssh/config',
-  line   => 'PasswordAuthentication no',
-  match  => '^PasswordAuthentication',
-  ensure => present,
-}
-
 file_line { 'Declare identity file':
+  ensure => present,
   path   => '/home/ubuntu/.ssh/config',
   line   => 'IdentityFile ~/.ssh/school',
   match  => '^IdentityFile',
+}
+
+file_line { 'Turn off passwd auth':
   ensure => present,
+  path   => '/home/ubuntu/.ssh/config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication',
 }
